18bb78a6ec4949f1ffa4671ea3bbd82c9139da23
Xiaomou Wang <wxm19890422@gmail.com>
2014-03-08 22:02:10 -0800

    range search

---
+++ BSTset.cc
@@ -0,0 +1,108 @@
+//http://www.careercup.com/question?id=16049662
+#include<iostream>
+#include<random>
+#include<queue>
+#include"BST_Node.h"
+
+using namespace std;
+
+
+class Solution {
+private:
+	Node *root = NULL;
+public:
+	void insert (int val) {
+		_insert(val, root);
+	}
+	Node * _insert (int val, Node *root) {
+		if (!root) {
+			root = new Node(val);
+			if (!this->root) this->root = root;
+			return root;
+		}
+		if (root->val > val) {
+			root->left_count++;
+			root->left = _insert(val, root->left);
+		}	else {
+			root->right_count++;
+			root->right = _insert(val, root->right);
+		}
+		return root;
+	}
+
+	void search_range(int a, int b) {
+		if (a > b) return;
+		do_serach_range(a, b, this->root);
+	}
+
+	int search_range_cnt(int a, int b) {
+		if (a > b) return 0;
+		return do_serach_range_cnt(a, b, this->root);
+	}
+
+	void do_serach_range(int a, int b, Node *root) {
+		if (!root) return;
+		if (root->val >= a) do_serach_range(a, b, root->left);
+		if (root->val >= a && root->val <=b) cout << root->val << " ";
+		if (root->val <= b) do_serach_range(a, b, root->right);
+	}
+
+	int do_serach_range_cnt(int a, int b, Node *root) {
+		if (!root) return 0;
+		cout << root->val << endl;
+		if (root->val <= a) {
+			if (root->right && root->right->val >= a) {
+				cout << "return left" << endl;
+				return root->right_count;
+			} 
+		} else if (root->val >= b) {
+			if (root->left && root->left->val <= b) {
+				cout << "return right" << endl;
+				return root->left_count;
+			} 
+		}
+		int l = 0;
+		int r = 0;
+		if (root->left && root->left->val >= a) l = do_serach_range_cnt(a, b, root->left);
+		if (root->right && root->right->val <= b) r = do_serach_range_cnt(a, b, root->right);
+		if (root->val >= a && root->val <= b)
+			return l + r + 1;
+		return l + r;
+	}
+
+	void travers() {
+		queue<Node *> q;
+		q.push(this->root);
+		int size = q.size();
+		while (q.size()) {
+			Node *tmp = q.front();
+			q.pop();
+			size--;
+			cout << tmp->val << " ";
+			if (tmp->left)
+				q.push(tmp->left);
+			if (tmp->right)
+				q.push(tmp->right);
+			if (!size) {
+				cout << endl;
+				size = q.size();
+			}
+		}
+	}		
+
+};
+
+
+int main() {
+	Solution sol;
+	for (int i = 0; i < 100; i += 3) {
+		sol.insert(i);
+	}
+//	sol.travers();
+
+	cout << "search in range 30--65 "  << endl;
+	sol.search_range(30, 65);
+	cout << endl;
+//	cout << "search in range 30--65 " << sol.search_range_cnt(30, 65) << endl;
+		
+}
