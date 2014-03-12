5eb2997a8b71a69085de2e5f52a77a3e55478152
Xiaomou Wang <wxm19890422@gmail.com>
2014-03-11 19:50:22 -0700

    mid hard

---
+++ zigzagOrder.cc
@@ -0,0 +1,45 @@
+//http://www.careercup.com/question?id=5724823657381888
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+	void reorder(vector<int> &input) {
+		int n = input.size();
+		if (!n) return;
+		for (int i = 1; i < n; i++) {
+			if (i % 2 == 0) {
+				if (input[i] > input[i - 1]) {
+					swap(input[i], input[i - 1]);
+				}
+			} else {
+				if (input[i] < input[i - 1]) {
+					swap(input[i], input[i - 1]);
+				}
+			}
+		}
+		return;
+	}
+	
+	void swap(int &a, int &b) {
+		int tmp = a;
+		a = b;
+		b = tmp;
+	}
+};
+
+
+int main() {
+	vector<int> input;
+	cout << "input distinct nums" << endl;
+	int tmp;
+	while (cin >> tmp) {
+		input.push_back(tmp);
+	}
+	Solution sol;
+	sol.reorder(input);
+	for (auto it = input.begin(); it != input.end(); it++) cout << *it << " ";
+	cout << endl;
+}
