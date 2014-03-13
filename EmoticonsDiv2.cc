90b4d921273e1cd115093c6cf83032213c8c83f2
Xiaomou Wang <wxm19890422@gmail.com>
2014-03-12 23:56:57 -0700

    hard

---
+++ EmoticonsDiv2.cc
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+#define MX 10000
+class EmoticonsDiv2 {
+	public:
+		int total;
+		int printSmiles(int smiles) {
+			if (smiles < 2) return 0;
+			vector<int> tmp(smiles + 1, MX);
+			vector<vector<int>> dp(smiles + 1, tmp);
+ 			total = smiles;
+			return 1 + get_smiles(smiles, 1, dp);
+		}
+		
+		int get_smiles(int smiles, int cur, vector<vector<int>> &dp) {
+			if (smiles == cur) return 0;
+			if (smiles < 0) return MX;
+			if (smiles < cur) return MX;
+			
+			int ans = MX;
+			if (dp[smiles][cur] != MX) return dp[smiles][cur];
+			
+			
+			if (smiles % cur == 0)
+				ans = min(ans, 1 + get_smiles(smiles - cur, cur, dp));
+			
+			int have = total - smiles + cur;
+		
+			ans = min(ans, 2 + get_smiles(total - have, have, dp));
+	
+			dp[smiles][cur] = ans;
+			return ans;
+			
+		}
+	
+};
